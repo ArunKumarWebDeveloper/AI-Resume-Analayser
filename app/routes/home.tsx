2aac5160018941ca266a6584a71679a5006efc8a
Arun <arunlokesh78@gmail.com>
2025-10-28 12:03:51 +0530

    updated-codes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -39,16 +39,16 @@ export default function Home() {
         loadResumes()
     }, []);
 
-    return <main className="bg-[url('/images/main.svg')] bg-cover">
+    return <main className="bg-[url('/images/bg3.jpg')] bg-cover bg-no-repeat">
         <Navbar />
 
         <section className="main-section">
             <div className="page-heading py-16">
-                <h1>Track Your Applications & Resume Ratings</h1>
+                <h1 >Track Your Applications & Resume Ratings</h1>
                 {!loadingResumes && resumes?.length === 0 ? (
-                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                    <h2 >No resumes found. Upload your first resume to get feedback.</h2>
                 ): (
-                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                    <h2 >Review your submissions and check AI-powered feedback.</h2>
                 )}
             </div>
             {loadingResumes && (
